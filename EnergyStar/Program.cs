07dd13674361f98a3b1ca7df0dbb9968671e449f
securitytest-debug <56504038+securitytest-debug@users.noreply.github.com>
2022-07-31 23:40:26 +0800

    Update Program.cs

--- EnergyStar/Program.cs
+++ EnergyStar/Program.cs
@@ -1,11 +1,4 @@
 using EnergyStar.Interop;
-using System;
-using System.Runtime.InteropServices;
-using System.Threading;
-using System.Diagnostics;
-using System.Collections.Generic;  
-using System.Text;  
-
 
 namespace EnergyStar
 {
@@ -14,32 +7,11 @@ namespace EnergyStar
         public delegate bool ConsoleCtrlDelegate(int CtrlType);  
         [DllImport("kernel32.dll")]
         private static extern bool SetConsoleCtrlHandler(ConsoleCtrlDelegate handlerRoutine, bool add);
-        private const int CtrlCEvent = 0;//CTRL_C_EVENT = 0;//一个Ctrl+C的信号被接收，该信号或来自键盘，或来自GenerateConsoleCtrlEvent    函数   
-        private const int CtrlBreakEvent = 1;//CTRL_BREAK_EVENT = 1;//一个Ctrl+Break信号被接收，该信号或来自键盘，或来自GenerateConsoleCtrlEvent    函数  
-        private const int CtrlCloseEvent = 2;//CTRL_CLOSE_EVENT = 2;//当用户系统关闭Console时，系统会发送此信号到此   
-        private const int CtrlLogoffEvent = 5;//CTRL_LOGOFF_EVENT = 5;//当用户退出系统时系统会发送这个信号给所有的Console程序。该信号不能显示是哪个用户退出。   
-        private const int CtrlShutdownEvent = 6;//CTRL_SHUTDOWN_EVENT = 6;//当系统将要关闭时会发送此信号到所有Console程序   
         static bool HandlerRoutine(int ctrlType)
         {
-            Console.WriteLine("Set    SetConsoleCtrlHandler    success!!");
-            switch (ctrlType)
-            {
-                case CtrlCEvent: System.Console.WriteLine("Ctrl+C keydown"); break;
-
-                case CtrlBreakEvent: System.Console.WriteLine("Ctrl+Break keydown"); break;
-
-                case CtrlCloseEvent: System.Console.WriteLine("window closed"); break;
-
-                case CtrlLogoffEvent: System.Console.WriteLine("log off or shut down"); break;
-
-                case CtrlShutdownEvent: System.Console.WriteLine("system shut down"); break;
-
-                default: System.Console.WriteLine(ctrlType.ToString()); break;
-            }
             cts.Cancel();
             HookManager.UnsubscribeWindowEvents();
             EnergyManager.RecoverAllUserProcesses();
-            Console.WriteLine($"bye");
             return false;
         }
         
